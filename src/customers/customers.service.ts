8a2110261916da1cc440481f2b40db13015e9a6c
Carlos Casas <carloscasasramirez@gmail.comckear>
2025-01-09 09:00:18 -0500

    save customer ready

--- src/customers/customers.service.ts
+++ src/customers/customers.service.ts
@@ -9,34 +9,76 @@ import { CustomerPoints } from './entities/customer-points.entity';
 
 @Injectable()
 export class CustomersService {
-
-   private queryRunner: QueryRunner
+  private queryRunner: QueryRunner;
   public constructor(
     @InjectRepository(Customer)
     private readonly customerRepository: Repository<Customer>,
     @InjectRepository(CustomerPoints)
     private readonly customerPointsrepository: Repository<CustomerPoints>,
   ) {}
+  /**
+   * @description Metodo encargado de guardar la información de los clientes junto con su puntaje inicial
+   * @param nit
+   * @param name
+   * @param points
+   * @returns error o data
+   */
   public async create(
     nit: string,
     name: string,
     points: number,
   ): Promise<ServiceInterface> {
+    this.queryRunner =
+      this.customerRepository.manager.connection.createQueryRunner();
+    await this.queryRunner.connect();
+    await this.queryRunner.startTransaction();
     try {
-      
+      const newCustomer = this.queryRunner.manager.create(Customer, {
+        nit,
+        name,
+      });
+
+      const savedCustomer = await this.queryRunner.manager.save(newCustomer);
+      const savedCustomerPoints = await this.saveCustomerPoints(
+        this.queryRunner,
+        savedCustomer.id,
+        points,
+      );
+      await this.queryRunner.commitTransaction();
       return {
         data: {
-          nit,
-          name,
-          points,
+          savedCustomer,
+          savedCustomerPoints,
         },
       };
     } catch (error) {
+      if (this.queryRunner.isTransactionActive) {
+        await this.queryRunner.rollbackTransaction();
+      }
       return {
         data: error.message,
         error: true,
       };
+    } finally {
+      await this.queryRunner.release();
     }
+  }
+
+  private async saveCustomerPoints(
+    queryRunner: QueryRunner,
+    customerId: number,
+    points: number,
+  ): Promise<CustomerPoints> {
+    const newCustomerPoints = queryRunner.manager.create(CustomerPoints, {
+      customer_id: customerId,
+      points: points,
+    });
+
+    const savedCustomerPoints = await queryRunner.manager.save(
+      newCustomerPoints,
+    );
+
+    return savedCustomerPoints;
   }
 
   findAll() {
